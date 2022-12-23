7bc121dd7997754a069cadee52a7e96420b5178c
jdev0x <119949928+jdev0x@users.noreply.github.com>
2022-12-23 13:47:39 +0000

    printing numbers from 0-9 using function, and loop

---
+++ code-025.asm
@@ -0,0 +1,20 @@
+MOV D, 2
+CALL print_numbers
+HLT
+
+print_numbers:
+    MOV A, 57
+    MOV B, 232
+    MOV C, 0    
+loop:
+    MOV [B],  A
+    DEC A
+    INC B
+    INC C
+    CMP C, 10
+    JE out_of_function
+    JMP loop
+
+
+out_of_function:
+    RET
