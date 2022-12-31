373bc2b958a1be9db38362a2c77d56e85f0077da
jdev0x <119949928+jdev0x@users.noreply.github.com>
2022-12-31 13:02:12 +0000

    Create code-042.asm

---
+++ code-042.asm
@@ -0,0 +1,36 @@
+;#include <stdio.h>
+
+;int main()
+;{
+;    int result = 75;
+;    if(result < 75){
+;        printf("A");
+;    }else if (result == 75){
+;        printf("B");
+;    }else{
+;        printf("C");
+;    }
+;    
+;    return 0;
+;}
+
+
+main:
+
+    MOV A, 75 
+    MOV B, 232 
+    
+    CM2P A, 75
+    JB print_a
+    CMP A, 75
+    JE print_b
+    MOV [B], 67
+    HLT
+    
+print_a:
+    MOV [B], 65
+    HLT
+    
+print_b:
+    MOV [B], 66
+    
