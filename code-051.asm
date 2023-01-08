b530fcf1714ddfab9cc117a24fa7c055f048b1a0
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-01-08 14:13:38 +0000

    string in assembly]]

---
+++ code-051.asm
@@ -0,0 +1,20 @@
+JMP code
+
+message: db "Welcome to Assembly"
+         db 0
+code:
+
+    MOV A, 232
+    MOV B, message
+loop:
+    MOV C, [B]
+    MOV [A], C
+    INC A
+    INC B
+    MOV D, [B]
+    CMP D, 0
+    JE out_of_code
+JMP loop
+
+out_of_code:
+    HLT
