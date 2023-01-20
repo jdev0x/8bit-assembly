15016fef40c0955c061d8289f5f10825abc16f07
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-01-20 13:07:03 +0000

    Update code-063.asm

--- code-063.asm
+++ code-063.asm
@@ -8,3 +8,16 @@ double:
     ADD A, A
     MOV D, A    
     RET
+
+; using MUL
+
+; input -> function -> output
+MOV A, 5
+CALL double
+MOV C, D
+HLT
+
+double:
+    MUL 2
+    MOV D, A    
+    RET
